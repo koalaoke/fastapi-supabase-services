5e0094fb73a9681ac912fb7670e8b1000242a889
Bernardo Brito <koalaokeah@outlook.com>
2025-12-11 10:31:43 -0300

    Adiciona User routes a aplicação

--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
+from src.routes.users import router as users_router
 
 app = FastAPI()
 
 @app.get("/")
 def health_check():
     return {"status": "API rodando"}
+
+app.include_router(users_router)
