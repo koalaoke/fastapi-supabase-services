d64e0cd0f016715fe0b29aa3005a546cda20bfb1
Bernardo Brito <koalaokeah@outlook.com>
2025-12-10 22:44:35 -0300

    Conserta nome da variável .env

--- src/database.py
+++ src/database.py
@@ -5,6 +5,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 url: str = os.environ.get("SUPABASE_URL")
-key: str = os.environ.get("SUPABASE_KEY")
+key: str = os.environ.get("SUPABASE_SERVICE_KEY")
 
 supabase: Client = create_client(url, key)
